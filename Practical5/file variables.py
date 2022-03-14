20f9e92a498d1c0b2cb52f4d3fde888f8e64a8ec
unknown <Your Name>
2022-03-14 12:22:49 +0100

    variables

---
+++ Practical5/file variables.py
@@ -0,0 +1,17 @@
+#desrib which year has the greastest number of new COVID-19
+a = 19245301
+b = 4218520
+c = 271
+d = b-c
+e = a-b
+if d>e:
+    print("the rate of new case in 2020 is greater than in 2021")
+elif d<e:
+    print("the rate of new case in 2021 is greater than in 2020")
+else:
+    print("the rate of new case in 2020 is the same sa in 2021")
+#w=x+y
+x = "I love "
+y = "the world"
+w = x +y
+print(w)
