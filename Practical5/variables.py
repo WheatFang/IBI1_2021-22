b0201053adc7a23d17283bf3b91e764fef69303b
unknown <Your Name>
2022-03-14 09:08:14 +0100

    wheat variables

---
+++ Practical5/variables.py
@@ -0,0 +1,11 @@
+a = 19245301
+b = 4218520
+c = 271
+d = b-c
+e = a-b
+if d>e:
+    print("the rate of new case in 2020 is greater than in 2021")
+elif d<e:
+    print("the rate of new case in 2021 is greater than in 2020")
+else:
+    print("the rate of new case in 2020 is the same sa in 2021")
