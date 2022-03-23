4735bf32c77b3fb5302b4e062a6a17d1c04c7b5b
unknown <Your Name>
2022-03-23 06:05:09 +0100

    visualising the effect of paternal age on offspring health

---
+++ Practical6/v-the effect of paternal age.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+N = 10
+x1 = [30,35,40,45,50,55,60,65,70,75]
+y1 = [1.03,1.07,1.11,1.17,1.23,1.32,1.42,1.55,1.72,1.94]
+plt.scatter(x1,y1,marker='o')
+plt.title("effect of paternal age")
+plt.show()
