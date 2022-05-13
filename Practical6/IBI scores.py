cb287e8dd069e8d35bc38873f66493526fadb095
unknown <Your Name>
2022-05-13 04:41:51 +0100

    fix

--- Practical6/IBI scores.py
+++ Practical6/IBI scores.py
@@ -1,7 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 scores = [45,36,86,57,53,92,65,45]
-plt.boxplot(scores,labels=["S"])
+print(scores)
+plt.boxplot(scores)
 plt.title("IBI scores")
 plt.axhline(60,color='b',linestyle='--',label='60')
 plt.show()
+average = np.mean(scores)
+if average >= 60:
+    print("the average mark is >60%")
+else:
+    print("the average mark is <60%")
