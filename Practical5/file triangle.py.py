8eb28cbad24a1264b3c456a937cc74db30796efc
unknown <Your Name>
2022-05-22 08:36:41 +0100

    after feedback

--- Practical5/file triangle.py.py
+++ Practical5/file triangle.py.py
@@ -1,6 +1,6 @@
-n = 10
+n = 1 # only one point on the top layer
 sum = 0
-while n > 0:
+while n < 11: # use a while lope to add 10 times
     sum = sum + n
-    n = n-1
-print(sum)
+    n = n+1 # Each layer has one fewer points than the last one
+    print(sum)
