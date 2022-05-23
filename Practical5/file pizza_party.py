7441b35bc04a1310e606b88ec42a04f32e86b2ed
unknown <Your Name>
2022-05-23 16:14:26 +0100

    feendback

--- Practical5/file pizza_party.py
+++ Practical5/file pizza_party.py
@@ -3,7 +3,7 @@ n=1 #n is the number of times to cut pizza, at least once
 p=(n*n+n+2)/2 # Cut n times, and you can get p pieces of pizza
 while (p<64):#loop ends when the number of pizza pieces is greater than the total number of people
     n=n+1 # Otherwise n plus one 
-    p=(n*n+n+2)/2
+    p=(n*n+n+2)/2 # p is the number of pieces of pizza
     print(n)
 print("the number is", str(n))
 # By trial and error method,
